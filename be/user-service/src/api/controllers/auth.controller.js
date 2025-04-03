965cb46ccb2aa215f11ab5b0236a4d453e6e0347
Anh Ha <haanh17722@gmail.com>
2025-04-03 11:30:06 +0700

    add more auth api

--- be/user-service/src/api/controllers/auth.controller.js
+++ be/user-service/src/api/controllers/auth.controller.js
@@ -33,6 +33,43 @@ class AuthController {
       metadata: { token, user: user.toJSON() },
     });
   }
+
+  async forgotPassword(req, res) {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw BadRequest("Email not found");
+    }
+
+    const newPassword = Math.random().toString(36).substring(2, 15);
+    user.password = newPassword;
+    await user.save();
+    // TODO: Send new password to user email instead of returning it
+    return OK({
+      res,
+      metadata: { newPassword },
+      message: "Password reset email sent",
+    });
+  }
+
+  async changePassword(req, res) {
+    const { email, password, newPassword } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw BadRequest("Email not found");
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      throw BadRequest("Invalid password");
+    }
+    user.password = newPassword;
+    await user.save();
+    return OK({
+      res,
+      metadata: {},
+      message: "Password changed successfully",
+    });
+  }
 }
 
 module.exports = new AuthController();
--- be/user-service/src/api/routes/auth.route.js
+++ be/user-service/src/api/routes/auth.route.js
@@ -5,5 +5,7 @@ const asyncHandler = require("express-async-handler");
 
 router.post("/register", asyncHandler(authController.register));
 router.post("/login", asyncHandler(authController.login));
+router.post("/forgot-password", asyncHandler(authController.forgotPassword));
+router.post("/change-password", asyncHandler(authController.changePassword));
 
 module.exports = router;
